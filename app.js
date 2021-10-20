ce4eb1c227b689acc8e697026a9e0798fb4c4475
jay-alln <benedictjay254@gmail.com>
2021-10-20 12:22:59 +0300

    changed style

--- app.js
+++ app.js
@@ -36,6 +36,32 @@ $(document).ready(function(){
 
 });
 
+// filterItmes
+
+
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("filter-item")){
+	   // deactivate existing active 'filter-item'
+	   filterContainer.querySelector(".active").classList.remove("active");
+	   // activate new 'filter-item'
+	   event.target.classList.add("active");
+	   const filterValue = event.target.getAttribute("data-filter");
+	   galleryItems.forEach((item) =>{
+	  if(item.classList.contains(filterValue) || filterValue === 'all'){
+		  item.classList.remove("hide");
+		   item.classList.add("show");
+	  }
+	  else{
+		  item.classList.remove("show");
+		  item.classList.add("hide");
+	  }
+	   });
+  }
+});
+
 // accordion
 let accordion = document.querySelectorAll(".label");
 
